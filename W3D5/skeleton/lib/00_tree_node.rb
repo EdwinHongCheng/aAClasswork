a35f94e173c7f050f5dc40e9d0623db359fbe142
Edwin Cheng <edwincheng3@gmail.com>
2020-12-04 12:02:08 -0800

    15 min pair programming w Bryce

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
+    attr_reader :value, :parent, :children
+    attr_writer :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent)
+        @parent = parent
+        parent.children << self if parent
+    end
+
 
 end
