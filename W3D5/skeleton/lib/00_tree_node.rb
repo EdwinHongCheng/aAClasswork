048a6fec673af2aeb186ab1dd050a69dccd22052
bcdguz <bcdguz96@gmail.com>
2020-12-04 13:53:30 -0800

    Worked on tree_node 15min swap

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -8,10 +8,55 @@ class PolyTreeNode
         @children = []
     end
 
-    def parent=(parent)
-        @parent = parent
-        parent.children << self if parent
+    def parent=(new_parent)
+
+        if @parent
+            @parent.children.delete(self)
+        end
+
+        @parent = new_parent
+        new_parent.children << self if new_parent
     end
 
+    def add_child(child_node)
+        child_node.parent = self
+        self.children << child_node if !self.children.include?(child_node)
+    end
 
+    def remove_child(child_node)
+        raise if child_node.parent == nil
+        child_node.parent = nil
+    end
+
+    def dfs(target_value)
+
+        return self if self.value == target_value
+        return nil if self.children.empty?
+        answer = nil
+
+        self.children.each do |child|
+            child_result = dfs(child)
+            if child_result != nil
+                answer = child_result
+            end
+        end
+
+        answer
+    end
 end
+
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+
+# # connect n3 to n1
+# n3.parent = n1
+# # connect n3 to n2
+# n3.parent = n2
+
+# # this should work
+# raise "Bad parent=!" unless n3.parent == n2
+# raise "Bad parent=!" unless n2.children == [n3]
+
+# # this probably doesn't
+# raise "Bad parent=!" unless n1.children == []
