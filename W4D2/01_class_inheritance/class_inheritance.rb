610a5340a89ec5cf16d15d89be9fd9e9743a5d26
Justin Cheasty <cheasty@gmail.com>
2020-12-08 14:19:36 -0800

    Finished class_inheritance project

--- W4D2/01_class_inheritance/class_inheritance.rb
+++ W4D2/01_class_inheritance/class_inheritance.rb
@@ -1,4 +1,6 @@
 class Employee
+    attr_reader :salary
+    
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
@@ -19,13 +21,23 @@ end
 class Manager < Employee
     attr_accessor :assigned_employees
 
-    def initialize
-
+    def initialize(name, title, salary, boss)
+        super
         @assigned_employees = []
     end
 
-    def bonus
-        # manager's bonus = (all assigned_employees' bonuses) * multiplier
+    def bonus(multiplier)
+        total = 0
+        queue = []
+        queue += @assigned_employees
+        while !queue.empty?
+            current_employee = queue.shift
+            total += current_employee.salary
+            if current_employee.is_a?(Manager)
+                queue += current_employee.assigned_employees
+            end
+        end
+        manager_bonus = total * multiplier
     end
 end
 
@@ -34,3 +46,7 @@ ned = Manager.new("Ned", "founder", 1000000, nil)
 darren = Manager.new("Darren", "TA Manager", 78000, ned)
 shawna = Employee.new("Shawna", "TA", 12000, darren)
 david = Employee.new("David", "TA", 10000, darren)
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
