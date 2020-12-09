fe76b62fbddc9e94e16bdfe85cb1c837d0a75dd5
Edwin Cheng <edwincheng3@gmail.com>
2020-12-09 11:06:20 -0800

    W4D3 Chess

---
+++ W4D2/02_chess_part_one/pieces.rb
@@ -0,0 +1,85 @@
+require "singleton"
+
+class Piece
+    attr_reader :color, :pos
+
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
+    end
+
+    def to_s
+        @color + self.to_s 
+    end
+
+    def empty? # if this is a null piece or not (false if not a Null piece)
+        self.is_a?(NullPiece)
+    end
+
+    def valid_moves #return the array of valid move pos
+
+    end
+end
+
+
+class NullPiece < Piece
+    include Singleton
+
+    def initialize
+        @null_piece = :X #placeholder code - delete late
+    end
+end
+
+
+
+class Rook < Piece
+    def initialize(color, board, pos)
+        super
+        @symbol = :R
+    end
+
+    private
+    def move_dirs
+
+    end
+end
+
+
+class Bishop < Piece
+    def initialize(color, board, pos)
+        super
+        @symbol = :B
+    end
+end
+
+
+class Queen < Piece
+    def initialize(color, board, pos)
+        super
+        @symbol = :Q
+    end
+end
+
+
+class Knight < Piece
+    def initialize(color, board, pos)
+        super
+        @symbol = :N
+    end
+end
+
+class King < Piece
+    def initialize(color, board, pos)
+        super
+        @symbol = :K
+    end
+end
+
+
+class Pawn < Piece
+    def initialize(color, board, pos)
+        super
+        @symbol = :p
+    end
+end
