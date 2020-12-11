635855568a2bfaed24786b2171788225dde69130
Edwin Cheng <edwincheng3@gmail.com>
2020-12-10 17:07:57 -0800

    W4D4 Classwork with Jeffrey Bogart

---
+++ W4D4/lib/first_tdd_project.rb
@@ -0,0 +1,123 @@
+# Would like to check if methods we don't want are being used
+
+class Array
+    def my_uniq
+        hash = Hash.new(0)
+        self.each { |ele| hash[ele] += 1 }
+        answer = []
+        hash.each_key { |ele| answer << ele }
+        answer
+    end
+
+    def two_sum
+        answer = []
+        self.each_with_index do |num1, idx1|
+            self.each_with_index do |num2, idx2|
+                if idx1 < idx2 && num1 + num2 == 0
+                    answer << [idx1, idx2]
+                end
+            end
+        end
+        answer
+    end
+end
+
+def my_transpose(matrix)
+    answer = []
+
+    matrix.each_with_index do |col, idx1|
+        new_row = []
+        matrix.each_with_index do |row, idx2|
+            new_row << matrix[idx2][idx1]
+        end
+        answer << new_row
+    end
+
+    answer
+end
+
+
+def stock_picker(stock_prices)
+    lowest_price = stock_prices[0]
+    lowest_day = 0
+
+    highest_price = stock_prices[0]
+    highest_day = 0
+
+    (1...stock_prices.length).each do |day|
+        if stock_prices[day] < lowest_price
+            lowest_price = stock_prices[day]
+            lowest_day = day
+        end
+    end
+
+    (1...stock_prices.length).each do |day|
+        if stock_prices[day] > highest_price && day > lowest_day
+            highest_price = stock_prices[day]
+            highest_day = day
+        end
+    end
+
+
+    return nil if lowest_day > highest_day
+    [lowest_day, highest_day]
+end
+
+class HanoiTowers
+    attr_reader :stacks, :size
+    
+    def initialize(num)
+        raise "Number must be 1 or greater" if num < 1
+        @size = num
+        @stacks = Array.new(3) { Array.new}
+        (1..num).each { |disc| @stacks[0] << disc }
+    end
+
+    def move_disk
+        puts "Enter the stack number of the stack you would like to take a disk from (0, 1, or 2)"
+
+        chosen_stack = gets.chomp.to_i
+        while stacks[chosen_stack].empty? || stacks[chosen_stack] == nil
+            puts "Invalid stack chosen"
+            puts "Enter the stack number of the stack you would like to take a disk from (0, 1, or 2)"
+            chosen_stack = gets.chomp.to_i
+        end
+
+        chosen_disk = stacks[chosen_stack].shift
+        puts "Enter the stack number of the new stack you would like to place the disk"
+        target_stack = gets.chomp.to_i
+        while target_stack == chosen_stack || stacks[target_stack] == nil || (chosen_disk > stacks[target_stack][0] if !stacks[target_stack].empty?)
+            puts "Invalid stack chosen"
+            puts "Enter the stack number of the new stack you would like to place the disk"
+            target_stack = gets.chomp.to_i
+        end
+
+        stacks[target_stack].unshift(chosen_disk)
+    end
+
+    def won?
+        (stacks[1] == (1..size).to_a) || (stacks[2] == (1..size).to_a)
+    end
+
+    def render
+        puts
+        stacks.each { |row| p row.reverse } 
+        puts
+    end
+
+    def play
+        while !won?
+            render
+            move_disk
+        end
+        puts
+        puts "Congrats! You won, buddy!"
+        puts "How about trying #{@size+1} disks next time?"
+        puts
+    end
+
+end
+
+
+game = HanoiTowers.new(1)
+game.play
---
+++ W4D4/spec/first_tdd_project_spec.rb
@@ -0,0 +1,99 @@
+require "rspec"
+require "first_tdd_project"
+
+describe 'Array#my_uniq' do
+
+    #want to test if my_uniq doesn't use Array#uniq
+
+    it "removes duplicates from an array" do
+        expect([1, 2, 1, 3, 3].my_uniq.length).to eq(3)
+    end
+
+    it "returns the unique elements in the order in which they first appeared" do
+        expect([7, 7, 8, 8, 9, 9, 9, 0].my_uniq).to eq([7, 8, 9, 0])
+    end
+
+end
+
+describe 'Array#two_sum' do
+
+    it "finds all pairs of positions where the elements at those positions sum to zero" do
+        expect([-1, 0, 1].two_sum).to eq([[0, 2]])
+    end
+
+    it "returns the index pairs with the smaller index first" do
+        expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+    end
+
+end
+
+describe "#my_transpose" do
+
+    #want to test if my_transpose doesn't use Array#transpose
+
+    let(:matrix) { [[0, 1, 2],[3, 4, 5],[6, 7, 8]] }
+
+    it "returns a tranposed matrix" do
+        expect(my_transpose(matrix)).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+    end
+
+end
+
+describe "#stock_picker" do
+    let(:stock_prices) { [0, 99, 100] } 
+
+    it "returns a pair of numbers" do
+        expect(stock_picker(stock_prices).length).to eq(2)
+    end
+
+    it "returns an ordered pair of days" do
+        expect(stock_picker(stock_prices)[0] < stock_picker(stock_prices)[1]).to eq(true)
+    end
+
+end
+
+describe HanoiTowers do
+    subject(:towers) { HanoiTowers.new(5) }
+
+    describe "#initialize" do
+
+        it "sets stacks to a 2D array with three subarrays" do
+            expect(towers.stacks.length).to eq(3)
+        end
+
+        it "sets a specified number of stacks to the first subarray" do
+            expect(towers.stacks[0]).to eq([1, 2, 3, 4, 5])
+        end
+
+    end
+
+    describe "#move_disk" do
+        it "takes the top disk from a stack and places the chosen disk on top of another stack" do
+            towers.move_disk
+            expect(towers.stacks == [[2, 3, 4, 5],[1],[]] || towers.stacks == [[2, 3, 4, 5],[],[1]]).to eq(true)
+        end
+
+        it "never has larger disks on top of smaller disks" do
+            towers.move_disk
+            expect(towers.stacks[0]).to eq(towers.stacks[0].sort)
+            expect(towers.stacks[1]).to eq(towers.stacks[1].sort)
+            expect(towers.stacks[2]).to eq(towers.stacks[2].sort)
+        end
+
+        it "places the disk on another stack" do
+            stack1 = towers.stacks[0].dup
+            towers.move_disk
+            expect(towers.stacks[0]).to_not eq(stack1)
+        end
+
+    end
+
+    describe "#won" do
+        it "returns true if all the disks are on a single stack other than the starting stack" do
+            expect(towers.won?).to eq(false)
+            towers.stacks[0], towers.stacks[2] = towers.stacks[2], towers.stacks[0]
+            expect(towers.won?).to eq(true)
+        end
+    end
+
+end
