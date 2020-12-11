b468fa441f225516eb364ee91eee660ea0cfef3c
appkenny <kzeng.95@gmail.com>
2020-12-11 13:34:20 -0800

    Started anagrams

--- W4D5/02_anagrams.rb
+++ W4D5/02_anagrams.rb
@@ -1,9 +1,18 @@
 # Phase 1
 
-def anagram?(str1, str2)
-    arr.permutation((arr.length)).include?(str)
+# def anagram?(str1, str2)
+#     arr1 = str1.split("")
+
+#     anagrams = arr1.permutation((arr1.length)).to_a.map { |sub_arr| sub_arr.join("")}
+#     anagrams.include?(str2)
+
+# end
+
+# Phase 2
+def second_anagram?
+
 end
 
 # Tests
-p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+# p anagram?("gizmo", "sally")    #=> false
+# p anagram?("elvis", "lives")    #=> true
