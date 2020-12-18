8986d738aed4e752e08aa8de176c1e2e2f7b4e51
Edwin Cheng <edwincheng3@gmail.com>
2020-12-17 16:53:23 -0800

    Finished Phase 2

--- W5D4/URLShortener/app/models/shortened_url.rb
+++ W5D4/URLShortener/app/models/shortened_url.rb
@@ -9,7 +9,28 @@
 #  short_url  :string           not null
 #  user_id    :integer          not null
 #
+
+require 'securerandom'
+
 class ShortenedUrl < ApplicationRecord
     validates :short_url, presence: true, uniqueness: true
     validates :long_url, presence: true, uniqueness: true
+
+    def self.random_code
+        short = SecureRandom.urlsafe_base64
+
+        while ShortenedUrl.exists?(:short_url => short)
+            short = SecureRandom.urlsafe_base64
+        end
+        short
+    end
+
+    def self.generate_short_url(user, long_url)
+        ShortenedUrl.create!(long_url: long_url, short_url: ShortenedUrl.random_code, user_id: user.id)
+    end
+
+    belongs_to :submitter,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
 end
