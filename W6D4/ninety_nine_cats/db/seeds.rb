9f728ffc8c81fe25240414a27692aeda1d722b82
Edwin Cheng <edwincheng3@gmail.com>
2020-12-31 15:45:46 -0800

    Almost done w index show pages

--- W6D4/ninety_nine_cats/db/seeds.rb
+++ W6D4/ninety_nine_cats/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# can use this in Rails Console:
+
+# kitty1 = Cat.new(birth_date: "2016/1/1", color: "blue", name: "Waffle", sex: "M", description: "I'm delicious!")
+# kitty2 = Cat.new(birth_date: "1999/12/20", color: "red", name: "Jazz", sex: "F", description: "A cool cat.")
