c747911e3b00528b366fc3e8a13e1656e2eb460a
Edwin Cheng <edwincheng3@gmail.com>
2021-01-26 14:16:11 -0800

    Started W10D2 Pairing

---
+++ W10D2/skeleton/widgets.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(
+        <main>
+            <div>"Hello World"</div>
+        </main>,
+
+        root);
+});
+
+console.log("Webpack is running!");
